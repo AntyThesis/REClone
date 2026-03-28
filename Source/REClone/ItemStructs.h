c014fddcd5e49b4a18c4ecc8e2ae4520ad925f6d
AntyThesis <163203888+AntyThesis@users.noreply.github.com>
2026-03-28 09:39:07 -0700

    Item struct added

---
+++ Source/REClone/ItemStructs.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemStructs.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FItemData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FText ItemName;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UTexture2D* ItemIcon;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UStaticMesh* ItemMesh;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	
+};
